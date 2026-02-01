90c5c5b45fd3a18b9327c7e0beb8837482a9eebd
Luis Baroni <baroniluishenrique@gmail.com>
2026-02-01 17:40:43 -0300

    feat: adds collaborators model

---
+++ src/collaborators/models/collaborator.ts
@@ -0,0 +1,65 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../../config/database";
+
+class Collaborator extends Model {
+  declare id: string;
+  declare name: string;
+  declare email: string;
+  declare city: string;
+  declare company: string;
+  declare createdAt: Date;
+}
+
+Collaborator.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Name cannot be empty",
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Invalid email format",
+        },
+        notEmpty: {
+          msg: "Email cannot be empty",
+        },
+      },
+    },
+    city: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    company: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      field: "created_at",
+    },
+  },
+  {
+    sequelize,
+    tableName: "collaborators",
+    modelName: "Collaborator",
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+export default Collaborator;
